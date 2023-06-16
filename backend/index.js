b79cd0cea30797a72b7ca93e48e7a3a80059581b
Sobhan Sai Kuriti <sobhansaikuriti03@gmail.com>
2023-06-16 18:20:11 +0530

    Final Changes

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const axios = require("axios");
+
+dotenv.config()
 
 const app = express();
 app.use(express.json());
@@ -7,7 +11,17 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body;
-    return res.json({ username: username, secret: "sha256..." });
+
+    try {
+        const r = await axios.put(
+            "https://api.chatengine.io/users/",
+            { username: username, secret: username, first_name: username },
+            { headers: { "private-key": process.env.PRIVATE_KEY } }
+        )
+        return res.status(r.status).json(r.data);
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data)
+    }
 });
 
 app.listen(3001);
---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,14 @@
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(import.meta.env.VITE_PROJECT_ID,props.user.username, props.user.secret)
+    return (
+    <div style={{height:'100vh'}}>
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow {...chatProps} style={{height:'100%'}} />
+    </div>
+    )
+}
+
+export default ChatsPage;
